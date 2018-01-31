5e62c84874df337895adcb1ed2e80a971692a666
henryq95 <quinteros7766@gmail.com>
2018-01-31 13:23:55 -0800

    python with node & express hello world

---
+++ public/helloworld.py
@@ -0,0 +1,5 @@
+import sys
+
+data = "Hello World from Python"
+print(data)
+sys.stdout.flush()
--- server.js
+++ server.js
@@ -1,24 +1,41 @@
 const express = require('express'),
-      path = require('path'),
-      bodyParser = require('body-parser'),
-      cors = require('cors'),
-      mongoose = require('mongoose'),
-      config = require('./config/DB'),
-      itemRoutes = require('./expressRoutes/itemRoutes');
+  path = require('path'),
+  bodyParser = require('body-parser'),
+  cors = require('cors'),
+  mongoose = require('mongoose'),
+  config = require('./config/DB')
 
-      mongoose.Promise = global.Promise;
-      mongoose.connect(config.DB).then(
-          () => {console.log('Database is connected') },
-          err => { console.log('Can not connect to the database'+ err)}
-        );
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DB).then(
+  () => { console.log('Database is connected') },
+  err => { console.log('Can not connect to the database' + err) }
+);
 
-      const app = express();
-      app.use(express.static('public'));
-      app.use(bodyParser.json());
-      app.use(cors());
-      app.use('/items', itemRoutes);
-      const port = process.env.PORT || 4000;
+const app = express();
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(cors());
+const port = process.env.PORT || 4000;
 
-      const server = app.listen(port, function(){
-        console.log('Listening on port ' + port);
+
+
+app.get('/python', (req, res) => {
+  
+      var spawn = require("child_process").spawn;
+      var process = spawn('python',['./public/helloworld.py']);
+  
+      process.stdout.on('data', function(data) {
+          console.log(data.toString());
+          res.write(data);
+          res.end('end');
+  
       });
+  })
+
+  app.get('/sample', function (req, res) {
+    res.send('this is a sample!');
+  });
+
+const server = app.listen(port, function () {
+  console.log('Listening on port ' + port);
+});
