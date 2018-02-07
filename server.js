c5a09d23bc468e91f80416fe3e1f51eaec46cff8
henryq95 <quinteros7766@gmail.com>
2018-02-07 13:08:08 -0800

    testing connection to mongodb

--- server.js
+++ server.js
@@ -11,12 +11,35 @@ mongoose.connect(config.DB).then(
   err => { console.log('Can not connect to the database' + err) }
 );
 
+var nameSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String
+ });
+ var User = mongoose.model("User", nameSchema);
+
+
 const app = express();
 app.use(express.static('public'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+
 const port = process.env.PORT || 4000;
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/nodemongo.html");
+ });
+
+ app.post("/addname", (req, res) => {
+  var myData = new User(req.body);
+  myData.save()
+  .then(item => {
+  res.send("item saved to database");
+  })
+  .catch(err => {
+  res.status(400).send("unable to save to database");
+  });
+ });
 
 
 app.get('/python', (req, res) => {
