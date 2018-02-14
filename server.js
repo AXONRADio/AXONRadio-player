76241a11d0a1d24045831efdd3fbeb0f8a76fe38
henryq95 <quinteros7766@gmail.com>
2018-02-13 20:16:24 -0800

    added sign up and login authentication

--- server.js
+++ server.js
@@ -1,24 +1,69 @@
 const express = require('express'),
-      path = require('path'),
-      bodyParser = require('body-parser'),
-      cors = require('cors'),
-      mongoose = require('mongoose'),
-      config = require('./config/DB'),
-      itemRoutes = require('./expressRoutes/itemRoutes');
+  path = require('path'),
+  bodyParser = require('body-parser'),
+  cors = require('cors'),
+  mongoose = require('mongoose'),
+  config = require('./config/DB')
 
-      mongoose.Promise = global.Promise;
-      mongoose.connect(config.DB).then(
-          () => {console.log('Database is connected') },
-          err => { console.log('Can not connect to the database'+ err)}
-        );
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 
-      const app = express();
-      app.use(express.static('public'));
-      app.use(bodyParser.json());
-      app.use(cors());
-      app.use('/items', itemRoutes);
-      const port = process.env.PORT || 4000;
 
-      const server = app.listen(port, function(){
-        console.log('Listening on port ' + port);
-      });
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DB).then(
+  () => { console.log('Database is connected') },
+  err => { console.log('Can not connect to the database' + err) }
+);
+
+var db = mongoose.connection;
+
+const app = express();
+
+//use sessions for tracking logins
+app.use(session({
+  secret: 'work hard',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
+
+
+// serve static files from template
+app.use(express.static('public'));
+
+app.use(cors());
+
+// parse incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// include routes
+var routes = require('./routes/router');
+app.use('/', routes);
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('File Not Found');
+  err.status = 404;
+  next(err);
+});
+
+
+// error handler
+// define as the last app.use callback
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.send(err.message);
+});
+
+
+
+const port = process.env.PORT || 4000;
+
+const server = app.listen(port, function () {
+  console.log('Listening on port ' + port);
+});
